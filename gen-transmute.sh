633baf3e1ac16ab3bcdaa698844b9fd1f5917ead
Matthew Baggett <matthew@baggett.me>
2020-03-19 21:59:17 +0000

    multiarch fiddling

--- gen-transmute.sh
+++ gen-transmute.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 TARGET=$1
-cat .github/workflows/build-x86_64-marshall.yml \
+
+cat .github/workflows/build-x86_64.yml \
     | sed "s|x86_64|${TARGET}|g" \
     | sed "s|/marshall|/marshall-${TARGET}|g" \
-    > .github/workflows/build-${TARGET}-marshall.yml
-
-cat .github/workflows/build-x86_64-php.yml \
-    | sed "s|x86_64|${TARGET}|g" \
     | sed "s|/php|/php-${TARGET}|g" \
-    > .github/workflows/build-${TARGET}-php.yml
+    > .github/workflows/build-${TARGET}.yml
 
